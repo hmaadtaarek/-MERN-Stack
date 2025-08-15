fa56435547e18a66b651bfd0ca4e0a85a7180c9f
HammadTarek <hammadjutthj6355@gmail.com>
2025-08-15 17:47:18 +0500

    user  routes initialized

--- src/app.js
+++ src/app.js
@@ -14,5 +14,13 @@ app.use(express.urlencoded({extended:true, limit: "16kb"}))
 app.use(cookieParser())
 app.use(express.static("public"))
 
+// router import
+import userRouter from "./routes/user.routes.js"
+
+
+
+// routes
+app.use("/api/v1/users" , userRouter)
+
 
 export {app}
